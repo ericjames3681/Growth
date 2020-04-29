72ef6942fd795b8afcb65cdc767456f8d44dd2e2
apple <apple@MacBook.attlocal.net>
2020-04-29 10:48:12 -0500

    heroku maintenace...

--- controllers/plants.js
+++ controllers/plants.js
@@ -11,10 +11,9 @@ async function index(req, res) {
   console.log(req.user);
   try {
     const plants = await Plant.find({ owners: { "$in": req.user._id } }).populate("owners");
-    console.log(plants);
     res.json(plants);
   } catch (err) {
-    console.log(err);
+
     res.status(500).json(err);
   }
 }
@@ -34,10 +33,3 @@ async function deleteOne(req, res) {
   const deletedPlant = await Plant.findByIdAndRemove(req.params.id);
   res.status(200).json(deletedPlant);
 }
-
-// async function update(req, res) {
-//   const updatedPlant = await Plant.findByIdAndUpdate(req.params.id, req.body, {
-//     new: true,
-//   });
-//   res.status(200).json(updatedPlant);
-// }
